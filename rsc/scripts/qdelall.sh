72bebae332cae443d61a13f5f40b84840deeb7d2
ahheckel <heckelandreas@googlemail.com>
2012-10-06 19:18:51 +0200

    deleted:    rsc/scripts/LIA_to_LAS_conformed.mat  	deleted:    rsc/scripts/apply_func2MNI.sh  	deleted:    rsc/scripts/apply_mc+unwarp.sh  	deleted:    rsc/scripts/apply_unwarp.sh  	deleted:    rsc/scripts/bin/sort8  	deleted:    rsc/scripts/checks.cmd  	deleted:    rsc/scripts/collectvols.sh  	deleted:    rsc/scripts/convert_regs.sh  	deleted:    rsc/scripts/createALFF.sh  	deleted:    rsc/scripts/cycler.sh  	deleted:    rsc/scripts/delbrokenlinks.sh  	deleted:    rsc/scripts/denoise4D.sh  	deleted:    rsc/scripts/dualreg.sh  	deleted:    rsc/scripts/dummy_bvalbvec.sh  	deleted:    rsc/scripts/eddy_correct.sh  	deleted:    rsc/scripts/eddy_correct_plot.sh  	deleted:    rsc/scripts/ersatzblade.launcher.sh  	deleted:    rsc/scripts/extract.sh  	deleted:    rsc/scripts/extractConfoundsFromNativeFuncs.sh  	deleted:    rsc/scripts/extractmerge.sh  	deleted:    rsc/scripts/extractmerge_parallel.sh  	deleted:    rsc/scripts/feat_T1_2_MNI.sh  	deleted:    rsc/scripts/feat_hpf.sh  	deleted:    rsc/scripts/feat_mask.sh  	deleted:    rsc/scripts/feat_scale.sh  	deleted:    rsc/scripts/feat_smooth.sh  	deleted:    rsc/scripts/feat_unwarp.sh  	deleted:    rsc/scripts/feat_writeMNI.sh  	deleted:    rsc/scripts/findclusters.sh  	deleted:    rsc/scripts/fs_convert.sh  	deleted:    rsc/scripts/fs_create_masks.sh  	deleted:    rsc/scripts/fsl_sub_NOPOSIXLY.sh  	deleted:    rsc/scripts/fsl_sub_v5_NOPOSIXLY.sh  	deleted:    rsc/scripts/getsliceorderSIEMENS_interleaved.sh  	deleted:    rsc/scripts/globalfuncs  	deleted:    rsc/scripts/globalvars_test  	deleted:    rsc/scripts/greperror.sh  	deleted:    rsc/scripts/greptracstats.sh  	deleted:    rsc/scripts/ic011_ExtractCorrelations.sh  	deleted:    rsc/scripts/longt_fslvbm_2_template.sh  	deleted:    rsc/scripts/longt_fslvbm_3_proc.sh  	deleted:    rsc/scripts/main.sh  	deleted:    rsc/scripts/make_fmap.sh  	deleted:    rsc/scripts/melodic.sh  	deleted:    rsc/scripts/nautilus/FSL_Glm  	deleted:    rsc/scripts/nautilus/FSL_checkTBSS  	deleted:    rsc/scripts/nautilus/FSL_checkVBM  	deleted:    rsc/scripts/nautilus/FSL_fslview  	deleted:    rsc/scripts/nautilus/FS_freeview  	deleted:    rsc/scripts/qdelall.sh  	deleted:    rsc/scripts/qstatloop.sh  	deleted:    rsc/scripts/rem_noise.sh  	deleted:    rsc/scripts/show3D.sh  	deleted:    rsc/scripts/show4D.sh  	deleted:    rsc/scripts/topup.sh  	deleted:    rsc/scripts/unwarp4D.sh  	deleted:    rsc/scripts/update_FSL-scripts.sh

--- rsc/scripts/qdelall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-for i in $(qstat -f |grep $(whoami | cut -c 1-10) | awk '{print $1}') ; do qdel $i ; done
-
--- rsc/scripts/qstatloop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-key=""
-while [ x$key != "xq" ] ; do clear; qstat -f ; read -t 2 -n 1 key ; done
-echo ""
-
