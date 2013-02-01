c7e333e7a521c90e1e9c708207e2189fb087bd57
ahheckel <heckelandreas@googlemail.com>
2013-02-01 13:32:12 +0100

    modified:   rsc/scripts/grepclusters.sh

--- rsc/scripts/grepclusters.sh
+++ rsc/scripts/grepclusters.sh
@@ -5,18 +5,19 @@
 # University of Heidelberg
 # heckelandreas@googlemail.com
 # https://github.com/ahheckel
-# 11/18/2012
+# 02/01/2013
 
 trap 'echo "$0 : An ERROR has occured."' ERR
 
 
 Usage() {
     echo ""
-    echo "Usage: `basename $0` <atlas:-tbss|-vbm|-ica> <dir> <search-pttrn> <thres> [<showall 1|0>] [<fslview 1|0>]"
-    echo "Example: `basename $0` -vbm ./stats \"*_corrp_*\" 0.95 1"
-    echo "         `basename $0` -vbm ./stats \"*_tfce_corrp_*\" -1"
-    echo "         NOTE: thres=-1     reports only the most significant result."
-    echo "               thres=-0.95  reports only the most significant result > 0.95."
+    echo "Usage: `basename $0` <atlas:-tbss|-vbm|-ica|-na> <dir> <search-pttrn> <thres> [<showall 1|0>] [<fslview 1|0>]"
+    echo "Example: `basename $0` -vbm ./stats \"*_corrp_*\" 0.95"
+    echo "         `basename $0` -ica ./stats \"*_tfce_corrp_*\" -0.95"
+    echo "         `basename $0` -na ./stats \"*_corrp_*\" -1"
+    echo "         NOTE: thres=-1     reports only the largest cluster."
+    echo "               thres=-0.95  reports only the largest cluster > 0.95."
     echo ""
     exit 1
 }
@@ -50,7 +51,8 @@ files=`find $dir -name "$pttrn" | grep -v SEED | $(dirname $0)/bin/sort8 -V`
 # display cluster-information
 echo "*** thres > $thres ***" | tee -a $logfile
 
-for f in $files ; do 
+for f in $files ; do
+  if [ $(imtest $f) -eq 0 ] ; then continue ; fi
   cluster --in=$f -t $thres --mm > $tmpfile
   nl=$(cat $tmpfile | wc -l)
   if [ $nl -gt 1 ] ; then
@@ -65,23 +67,28 @@ for f in $files ; do
       x="$(echo $line | cut -d " " -f 4)"
       y="$(echo $line | cut -d " " -f 5)"
       z="$(echo $line | cut -d " " -f 6)"
-      
       max=$(printf '%0.3f' $max)
-      #tval=$(fslmeants -i $(echo $f | sed "s|tfce_||g" | sed "s|corrp_||g" | sed "s|p_||g" | sed "s|vox_||g") --usemm -c $x $y $z) # query t-value
-      tval=$(fslmeants -i $(echo $f | sed "s|_tfce_|_|g" | sed "s|_corrp_|_|g" | sed "s|_p_|_|g" | sed "s|_vox_|_|g") --usemm -c $x $y $z) # query t-value
+      tvalfile=$(echo $f | sed "s|_tfce_|_|g" | sed "s|_corrp_|_|g" | sed "s|_p_|_|g" | sed "s|_vox_|_|g")
       
+      # query t-value
+      if [ $(imtest $tvalfile) -eq 1 ] ; then
+        tval=$(fslmeants -i $tvalfile --usemm -c $x $y $z) 2>/dev/null
+      else
+        tval=-999
+      fi
+      
+      # display
+      printf '   %5.3f t/f=%4.2f (%5i) at [ %5.1f %5.1f %5.1f ] (mm) \n' $max $tval $size $x $y $z | tee -a $logfile
       if [ $anal = "-tbss" ] ; then
         JHU1=$(atlasquery  -a "JHU ICBM-DTI-81 White-Matter Labels" -c ${x},${y},${z} | cut -d ">" -f 4)
-        JHU2=$(atlasquery  -a "JHU White-Matter Tractography Atlas" -c ${x},${y},${z} | cut -d ">" -f 4)      
-        printf '   %5.3f t/f=%4.2f (%5i) at [ %5.1f %5.1f %5.1f ] (mm) \n' $max $tval $size $x $y $z | tee -a $logfile
+        JHU2=$(atlasquery  -a "JHU White-Matter Tractography Atlas" -c ${x},${y},${z} | cut -d ">" -f 4)        
         printf '\t JHU1: %s\n' "$JHU1" | tee -a $logfile
         printf '\t JHU2: %s\n' "$JHU2" | tee -a $logfile
       fi
       if [ $anal = "-vbm" -o $anal = "-ica" ] ; then
         HAV1=$(atlasquery  -a "Harvard-Oxford Cortical Structural Atlas" -c ${x},${y},${z} | cut -d ">" -f 4)
         HAV2=$(atlasquery  -a "Harvard-Oxford Subcortical Structural Atlas" -c ${x},${y},${z} | cut -d ">" -f 4)
-        TAL=$(atlasquery  -a "Talairach Daemon Labels" -c ${x},${y},${z} | cut -d ">" -f 4)
-        printf '   %5.3f t/f=%4.2f (%5i) at [ %5.1f %5.1f %5.1f ] (mm) \n' $max $tval $size $x $y $z | tee -a $logfile
+        TAL=$(atlasquery  -a "Talairach Daemon Labels" -c ${x},${y},${z} | cut -d ">" -f 4)        
         printf '\t TAL:  %s \n' "$TAL" | tee -a $logfile
         printf '\t HAV1: %s \n' "$HAV1" | tee -a $logfile
         printf '\t HAV2: %s \n' "$HAV2" | tee -a $logfile
@@ -122,7 +129,7 @@ if [ $anal = "-vbm" ] ; then
       statsdir=$(dirname $f);
       if [ "$statsdir" = "." ] ; then statsdir=".." ; else statsdir=$(dirname $(dirname $f)) ; fi
       res=$(fslinfo $f | grep pixdim1 | awk {'print $2'}) ; res=$(printf '%.0f' $res)
-      fslview $statsdir/mean_GM_mod_merg_smoothed.nii.gz $f -l "Red" -b 0.75,0.9     
+      fslview $statsdir/mean_GM_mod_merg_smoothed.nii.gz $f -l "Red" -b ${thres},0.9     
     done
   fi
 fi
@@ -141,7 +148,7 @@ if [ $anal = "-ica" ] ; then
         else
           if [ $(echo $MNI | grep _4mm_ | wc -l) -eq 1 ] ; then rsn=${FSLDIR}/data/standard/rsn10_CSFWM_4mm.nii.gz ; fi
           if [ $(echo $MNI | grep _2mm_ | wc -l) -eq 1 ] ; then rsn=${FSLDIR}/data/standard/rsn10_CSFWM_2mm.nii.gz ; fi
-          fslview $MNI $rsn -t 0 -l "Blue-Lightblue" -b 1,2.1372 $f -l "Red" -b 0.95,1 -t 1
+          fslview $MNI $rsn -t 0 -l "Blue-Lightblue" -b 1,2.1372 $f -l "Red" -b ${thres},1 -t 1
           break
         fi        
       done # end MNI    
