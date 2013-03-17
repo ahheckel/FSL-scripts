41a3e8e1bf29cf2e38fad7411d4f41f91327f6e6
ahheckel <heckelandreas@googlemail.com>
2013-03-17 12:05:44 +0100

    renamed:    rsc/scripts/mni-vol2fsaverage-surf.sh -> rsc/scripts/vol2surf.sh

--- rsc/scripts/vol2surf.sh
+++ rsc/scripts/vol2surf.sh
@@ -12,8 +12,8 @@ set -e
 
 Usage() {
     echo ""
-    echo "Usage:   `basename $0` <input4D> <output4D>"
-    echo "Example: `basename $0` log_tfce_corrp_tstat1.nii.gz sig.mgh"
+    echo "Usage:    `basename $0` <input4D> <output4D> [<target-subject>] [<input2target_register>]"
+    echo "Example:  `basename $0` log_tfce_corrp_tstat1.nii.gz sig.mgh fsaverage mni2fsaverage.register.dat"
     echo ""
     exit 1
 }
@@ -22,9 +22,13 @@ Usage() {
 
 src="$1"
 output="$2"
+srcreg="$3"
+trgsubject="$4"
 
-srcreg=$(dirname $0)/mni2fsaverage.register.dat
-trgsubject=fsaverage
+if [ x"$srcreg" = "x" ] ; then srcreg=$(dirname $0)/mni2fsaverage.register.dat ; fi
+if [ x"$trgsubject" = "x" ] ; then trgsubject=fsaverage ; fi
+
+if [ ! -f $srcreg ] ; then echo "`basename $0` : '$srcreg' not found - exiting..." ; exit 1 ; fi
 
 mkdir -p $(dirname $output)
 
