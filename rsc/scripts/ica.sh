5b3b58e754cf366b019d16319613892ebe7413dd
ahheckel <heckelandreas@googlemail.com>
2013-05-05 02:03:15 +0200

    modified:   rsc/scripts/ica.sh

--- rsc/scripts/ica.sh
+++ rsc/scripts/ica.sh
@@ -2,12 +2,12 @@
 
 Usage() {
     echo ""
-    echo "Usage: `basename $0` <4dinput> <motion-corr:0|1> <FWHM(mm)> <hpf(s)|Inf> <TR(s)>"
+    echo "Usage: `basename $0` <4dinput> <motion-corr:0|1> <FWHM(mm)> <hpf(s)|Inf> <TR(s)> <resolution(mm)|0>"
     echo ""
     exit 1
 }
 
-[ "$5" = "" ] && Usage
+[ "$6" = "" ] && Usage
 
 # assign input arguments
 input="$(remove_ext $1)"
@@ -15,6 +15,7 @@ domc=$2
 smkrnl="$3" ; _smkrnl=$(echo $smkrnl | sed "s|\.||g") # remove '.'
 hpf="$4"
 TR="$5"
+res=$6
 
 # display info
 echo "`basename $0` : input:           $input"
@@ -22,6 +23,7 @@ echo "`basename $0` : motion-corr:     $domc"
 echo "`basename $0` : smooth(FWHM):    $smkrnl"
 echo "`basename $0` : hpf(s):          $hpf"
 echo "`basename $0` : TR(s):           $TR"
+echo "`basename $0` : resolution(mm):  $res"
 echo "---------------------------"
 
 # mid volume is...
@@ -32,13 +34,25 @@ idx=$(echo "scale=0; $total_volumes / 2" | bc)
 if [ $domc -eq 1 ] ; then
   cmd="mcflirt -in $input -out ${input}_mcf -refvol $idx"
   echo "`basename $0`: $cmd" ; $cmd
+  sminput=${input}_mcf
+else
+  sminput=${input} 
 fi
-if [ $domc -eq 1 ] ; then sminput=${input}_mcf ; else sminput=${input} ; fi
+
 cmd="$(dirname $0)/feat_smooth.sh ${sminput} ${sminput} $smkrnl $hpf $TR"
 echo "`basename $0`: $cmd" ; $cmd
 imrm ${sminput}_susan_mask.nii.gz
 rm ${sminput}_susan_mask_vals
-cmd="$(dirname $0)/melodic.sh ${sminput}_s${_smkrnl}_hpf${hpf} $TR -1"
+
+if [ $res -gt 0 ] ; then 
+  cmd="$(dirname $0)/resample.sh ${sminput}_s${_smkrnl}_hpf${hpf} $res ${sminput}_s${_smkrnl}_hpf${hpf}_res${res}"
+  echo "`basename $0`: $cmd" ; $cmd
+  icainput=${sminput}_s${_smkrnl}_hpf${hpf}_res${res}
+else 
+  icainput=${sminput}_s${_smkrnl}_hpf${hpf}
+fi
+
+cmd="$(dirname $0)/melodic.sh ${icainput} $TR -1"
 echo "`basename $0`: $cmd" ; $cmd
 
 # done
