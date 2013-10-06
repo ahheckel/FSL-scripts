6319280652cf52c53e6ee319952a1713018f0bc4
ahheckel <heckelandreas@googlemail.com>
2013-10-06 13:06:14 +0200

    modified:   rsc/scripts/extractmerge.sh  	modified:   rsc/scripts/split4D.sh

--- rsc/scripts/extractmerge.sh
+++ rsc/scripts/extractmerge.sh
@@ -14,12 +14,13 @@ trap 'echo "$0 : An ERROR has occured."' ERR
     
 Usage() {
     echo ""
-    echo "Usage: `basename $0` <out4D> <indices|all|mid> [<fslmaths unary operator>] <\"input files\"> <qsub logdir>"
-    echo "Example: `basename $0` ./chk/means.nii.gz 0,1,2,3 -Tmean \"\$inputs\" /tmp"
-    echo "         `basename $0` ./chk/bolds.nii.gz 0,1,2,3 none \"\$inputs\" /tmp"
+    echo "Usage: `basename $0` <out4D> <indices|all|mid> [<fslmaths unary operator>] <\"input files\"> [<qsub logdir>]"
+    echo "Example: `basename $0` ./chk/means.nii.gz [0,1,2:2:end-1] -Tmean \"\$inputs\" /tmp"
+    echo "         `basename $0` ./chk/means.nii.gz [0,1,2,3] -Tmean \"\$inputs\" /tmp"
+    echo "         `basename $0` ./chk/bolds.nii.gz [0,1,2,3] none \"\$inputs\" /tmp"
     echo "         `basename $0` ./chk/bolds.nii.gz 0 \"\$inputs\" /tmp"
-    echo "         `basename $0` ./chk/bolds.nii.gz all -Tmean \"\$inputs\" /tmp"
     echo "         `basename $0` ./chk/bolds.nii.gz mid \"\$inputs\""
+    echo "         `basename $0` ./chk/bolds.nii.gz all -Tmean \"\$inputs\" /tmp"
     echo ""
     exit 1
 }
@@ -68,17 +69,24 @@ function waitIfBusyIDs()
   rm $IDfile ; touch $IDfile
 }
 
-[ "$3" = "" ] && Usage
-
 # define vars
 out="$1"
+# rem commas and brackets
 idces="$(echo "$2" | sed 's|,| |g')"
-if [ $(echo $idces | wc -w) -gt 1 -o "$idces" = "all" ] ; then
+idces=$(echo $idces | cut -d ] -f 1)
+idces=$(echo $idces | cut -d [ -f 2)
+# more than one index ?
+if [ $(echo $idces | wc -w) -gt 1 -o "$idces" = "all" -o $(echo $idces | grep : | wc -l) -gt 0 ] ; then
   op="$3" ; shift
   if [ x"$op" = "xnone" ] ; then op=" " ; fi
+  multi=1
+else
+  multi=0
 fi
 inputs="$3"
 logdir="$4"
+
+[ "$3" = "" ] && Usage
 
 # create working dir.
 # Don't use mktemp because script is self-submitting.
@@ -105,21 +113,33 @@ rm -f ${out}.txt
 # extracting...
 n=0 ; i=1
 for input in $inputs ; do
-  if [ ! -f $input ] ; then echo "`basename $0`: '$input' not found." ; continue ; fi
+  if [ $(imtest $input) -eq 0 ] ; then echo "`basename $0`: WARNING: cannot read '$input' - continuing..." | tee -a ${out}.txt ; continue ; fi
+  nvol=`fslinfo  $input | grep ^dim4 | awk '{print $2}'` ; mid=$(echo "scale=0 ; $nvol / 2" | bc)
   if [ "$idces" = "all" ] ; then
       echo "`basename $0`: $i - applying unary fslmaths operator '$op' to '$input'..." | tee -a ${out}.txt
       fsl_sub -l $logdir fslmaths $input $op $wdir/_tmp_$(zeropad $n 4) >> $wdir/jid.list # apply operator
   elif [ "$idces" = "mid" ] ; then
-      nvol=`fslinfo  $input | grep ^dim4 | awk '{print $2}'` ; mid=$(echo "scale=0 ; $nvol / 2" | bc)
       echo "`basename $0`: $i - extracting volume at pos. $mid from '$input'..."  | tee -a ${out}.txt
       fsl_sub -l $logdir fslroi $input $wdir/_tmp_$(zeropad $n 4) $mid 1 >> $wdir/jid.list
   else
     for idx in $idces ; do
-      echo "`basename $0`: $i - extracting volume at pos. $idx from '$input'..."  | tee -a ${out}.txt
-      if [ $(echo $idces | wc -w) -gt 1 ] ; then
-        fsl_sub -l $logdir fslroi $input $wdir/_tmp_$(zeropad $n 4)_idx$(zeropad $idx 4) $idx 1 >> $wdir/jid.list
-      else
-        fsl_sub -l $logdir fslroi $input $wdir/_tmp_$(zeropad $n 4) $idx 1 >> $wdir/jid.list
+      idx=$(echo $idx | sed "s|mid|$mid|g") ; idx=$(echo $idx | sed "s|end|$[$nvol-1]|g")
+      if [ $(echo $idx | grep : | wc -l) -gt 0 ] ; then
+        field1=$(echo $idx | cut -d : -f 1); field1=$[$field1]
+        field2=$(echo $idx | cut -d : -f 2); field2=$[$field2]
+        field3=$(echo $idx | cut -d : -f 3); field3=$[$field3]
+        for idx in `seq $field1 $field2 $field3` ; do
+          echo "`basename $0`: $i - extracting volume at pos. $idx from '$input'..."  | tee -a ${out}.txt
+          fsl_sub -l $logdir fslroi $input $wdir/_tmp_$(zeropad $n 4)_idx$(zeropad $idx 4) $idx 1 >> $wdir/jid.list
+        done
+      else      
+        idx=$[$idx]
+        echo "`basename $0`: $i - extracting volume at pos. $idx from '$input'..."  | tee -a ${out}.txt
+        if [ $multi -eq 1 ] ; then
+          fsl_sub -l $logdir fslroi $input $wdir/_tmp_$(zeropad $n 4)_idx$(zeropad $idx 4) $idx 1 >> $wdir/jid.list
+        elif [ $multi -eq 0 ] ; then
+          fsl_sub -l $logdir fslroi $input $wdir/_tmp_$(zeropad $n 4) $idx 1 >> $wdir/jid.list
+        fi
       fi
     done    
   fi
@@ -130,20 +150,33 @@ done
 # waiting...
 waitIfBusyIDs $wdir/jid.list
 
-# if more than one index...
-if [ $(echo $idces | wc -w) -gt 1 ] ; then
+# apply fslmaths operator if more than one index...
+if [ $multi -eq 1 ] ; then
   n=0 ; rm -f $wdir/apply_operator.cmd
   echo "`basename $0`: merging (and applying unary fslmaths operator: '$op')..."  | tee -a ${out}.txt
   for input in $inputs ; do
-    if [ ! -f $input ] ; then continue ; fi
-    files=""
-    for idx in $idces ; do files=$files" "$wdir/_tmp_$(zeropad $n 4)_idx$(zeropad $idx 4) ; done
+    if [ $(imtest $input) -eq 0 ] ; then continue ; fi
+    files=""    
+    for idx in $idces ; do        
+      idx=$(echo $idx | sed "s|mid|$mid|g") ; idx=$(echo $idx | sed "s|end|$[$nvol-1]|g")
+      if [ $(echo $idx | grep : | wc -l) -gt 0 ] ; then
+        field1=$(echo $idx | cut -d : -f 1); field1=$[$field1]
+        field2=$(echo $idx | cut -d : -f 2); field2=$[$field2]
+        field3=$(echo $idx | cut -d : -f 3); field3=$[$field3]
+        for idx in `seq $field1 $field2 $field3` ; do
+          files=$files" "$wdir/_tmp_$(zeropad $n 4)_idx$(zeropad $idx 4)
+        done
+      else      
+        idx=$[$idx]
+        files=$files" "$wdir/_tmp_$(zeropad $n 4)_idx$(zeropad $idx 4)
+      fi
+    done
     echo "fslmerge -t $wdir/_tmp_$(zeropad $n 4) $files ; \
     imrm $files ; \
     fslmaths $wdir/_tmp_$(zeropad $n 4) $op $wdir/_tmp_$(zeropad $n 4)" >> $wdir/apply_operator.cmd
     n=$(echo "$n + 1" | bc)
   done
-  cat $wdir/apply_operator.cmd
+  echo -n "    " ; cat $wdir/apply_operator.cmd
   fsl_sub -l $logdir -t $wdir/apply_operator.cmd >> $wdir/jid.list
   # waiting...
   waitIfBusyIDs $wdir/jid.list
@@ -151,9 +184,16 @@ fi # end if
 
 # merging...
 echo "`basename $0`: merging to '${out}'..."  | tee -a ${out}.txt
-fsl_sub -l $logdir fslmerge -t ${out} $(imglob $wdir/_tmp_????.*) >> $wdir/jid.list
+cmd="fslmerge -t ${out} $(imglob $wdir/_tmp_????.*)"
+echo "    $cmd" | tee -a ${out}.txt
+fsl_sub -l $logdir "$cmd" >> $wdir/jid.list
 
 # waiting...
 waitIfBusyIDs $wdir/jid.list
 
+# list error log entries
+echo "`basename $0`: cat SGE *.e logs:"
+cat $logdir/*.e*
+
+# done
 echo "`basename $0`: done." 
--- rsc/scripts/split4D.sh
+++ rsc/scripts/split4D.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# i) Splits 4D volume in 4Ds of size n or ii) splits 4D volume according to split-vector and merges the split.
+# i) Splits 4D volume in 4Ds of size n or ii) splits 4D volume according to split-vector and merges the split if requested.
 
 # Written by Andreas Heckel
 # University of Heidelberg
@@ -15,18 +15,29 @@ Usage() {
     echo ""
     echo "Usage:   `basename $0` <input4D> <size> <output4D>"
     echo "         `basename $0` <input4D> <vector> <output4D>"
+    echo ""
+    echo "Options: -m    merge extraction to 4D"
+    echo ""
     echo "Example: `basename $0` in 128 out"
-    echo "         `basename $0` in [0:2:end-1] out"
+    echo "         `basename $0` -m in [1,3,5] out"
+    echo "         `basename $0` -m in [0:2:end-1] out"
+    echo "         `basename $0` -m in [0,2:2:end-1] out"
     echo ""
     exit 1
 }
 
+function row2col()
+{
+  local dat=`cat $1`
+  local i=""
+  for i in $dat ; do echo $i ; done
+}
+
+merge=0 ; if [ "$1" = "-m" ] ; then  merge=1 ; shift ; fi
 [ "$3" = "" ] && Usage
 
 # create working dir.
 tmpdir=$(mktemp -d -t $(basename $0)_XXXXXXXXXX) # create unique dir. for temporary files
-#tmpdir=/tmp/$(basename $0)_$$
-#mkdir -p $tmpdir
 
 # define exit trap
 trap "rm -f $tmpdir/* ; rmdir $tmpdir ; exit" EXIT
@@ -36,61 +47,78 @@ input=$(remove_ext "$1")
 n="$2"
 output=$(remove_ext "$3")
 
-# check
-if [ $(imtest $input) -eq 0 ] ; then echo "`basename $0`: Cannot read '$input'... exiting." ; exit 1 ; fi
-
 # declare vars
 nvols=`fslinfo $input | grep ^dim4 | awk '{print $2}'`
 
-# scalar or vector ?
-if [ $(echo $n | grep : | wc -l) -eq 1 ] ; then
-  vector=1
-  n=$(echo $n | sed "s|end|$[$nvols-1]|g")
-  field1=$(echo $n | cut -d : -f 1 | cut -c 2-); field1=$[$field1]
-  field2=$(echo $n | cut -d : -f 2); field2=$[$field2]
-  field3=$(echo $n | cut -d : -f 3 | cut -c 1- | cut -d ] -f 1); field3=$[$field3]
-else
-  vector=0
-fi
+# check input
+if [ $(imtest $input) -eq 0 ] ; then echo "`basename $0`: Cannot read '$input'... exiting." ; exit 1 ; fi
+
+# rem commas & brackets & tags
+n="$(echo "$n" | sed 's|,| |g')"
+n=$(echo $n | sed "s|end|$[$nvols-1]|g")
+n=$(echo $n | sed "s|mid|$(echo "$nvols / 2" | bc)|g")
+n=$(echo $n | cut -d ] -f 1)
+n=$(echo $n | cut -d [ -f 2)
+
+# vector or scalar ?
+_n=""
+for i in $(echo $n | row2col) ; do
+  if [ $(echo $i | grep : | wc -l) -eq 1 ] ; then
+     field1=$(echo $i | cut -d : -f 1); field1=$[$field1]
+     field2=$(echo $i | cut -d : -f 2); field2=$[$field2]
+     field3=$(echo $i | cut -d : -f 3); field3=$[$field3]
+     _n=$_n" "$(seq $field1 $field2 $field3)
+  else
+    _n=$_n" "$[$i]
+  fi
+done  
+if [ $(echo $_n | wc -w) -gt 1 ] ; then vector=1 ; else vector=0 ; fi
 
 if [ $vector -eq 0 ] ; then
   iter=$(echo "scale=0 ; $nvols / $n" | bc -l)
-  if [ $iter -eq 0 ] ; then echo "`basename $0`: ERROR: only $nvols voulmes in '$input'. Exiting." ; exit 1 ; fi
+  if [ $iter -eq 0 ] ; then echo "`basename $0`: ERROR: only $nvols volumes in '$input' (less than $n). Exiting." ; exit 1 ; fi
 
   # check
   _nvols=$(echo "scale=0; $iter*$n" | bc -l)
   resid=$(echo "scale=0; $nvols - $_nvols" | bc -l)
-  if [ $resid -gt 0 ] ; then echo "`basename $0`: WARNING: last 4D ('${output}_$(zeropad $iter 3)') will only have $resid volumes (not $n)." ; fi
+  if [ $resid -gt 0 ] ; then echo "`basename $0`: WARNING: last 4D ('${output}_$(zeropad $iter 4)') will only have $resid volumes (not $n)." ; fi
 
   # execute
   echo "`basename $0`:"
   for i in `seq 0 $[$iter-1]` ; do
     tmin=$(echo "scale=0; $i*$n" | bc -l)
-    cmd="fslroi $input ${output}_$(zeropad $i 3) $tmin $n"
+    cmd="fslroi $input ${output}_$(zeropad $i 4) $tmin $n"
     echo "    $cmd" ; $cmd
   done
   if [ $resid -gt 0 ] ; then
     i=$[$i+1]
     tmin=$(echo "scale=0; $i*$n" | bc -l)
-    cmd="fslroi $input ${output}_$(zeropad $i 3) $tmin $resid"
+    cmd="fslroi $input ${output}_$(zeropad $i 4) $tmin $resid"
     echo "    $cmd" ; $cmd
   fi
   
 elif [ $vector -eq 1 ] ; then
-  echo "`basename $0`: split-vector: [$field1:$field2:$field3] ; nvols: $nvols"
+  if [ $merge -eq 1 ] ; then echo "`basename $0`: will merge extraction." ; fi
+  echo "`basename $0`: nvols: $nvols ; split-vector: [$n]"
   # split
-  fslsplit $input ${tmpdir}/$(basename $output)_tmp
-  full_list=`imglob ${tmpdir}/$(basename $output)_tmp????.*`
+  fslsplit $input ${tmpdir}/$(basename $output)_
+  full_list=`imglob ${tmpdir}/$(basename $output)_????.*`
 
   i=0 ; files="" ; err=0
-  for i in `seq $field1 $field2 $field3` ; do
-    file=${tmpdir}/$(basename $output)_tmp$(zeropad $i 4)
+  for i in $_n ; do
+    file=${tmpdir}/$(basename $output)_$(zeropad $i 4)
     if [ $(imtest $file) -eq 0 ] ; then echo "`basename $0`: ERROR: Cannot read '$file'..." ; err=1 ; fi
     files=$files" "$file
   done
   if [ $err -eq 1 ] ; then echo "`basename $0`: Exiting..." ; exit 1 ; fi
-  cmd="fslmerge -t $output $files"
-  echo "    $cmd" ; $cmd
+  if [ $merge -eq 1 ] ; then
+    cmd="fslmerge -t $output $files"
+    echo "    $cmd" ; $cmd
+  else
+    for file in $files ; do
+      immv $file $(dirname $output)
+    done  
+  fi
 fi
 
 # done
