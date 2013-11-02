f279e5f02533ee5c092489660b8aacb31ed631d2
ahheckel <heckelandreas@googlemail.com>
2013-11-03 00:22:16 +0100

    deleted:    go.sh

--- go.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-trap 'echo -e "\n********************\nTo cancel background job type \n kill $(cat ./log | grep Job | cut -d : -f 2)\n\nTo view logfile type \n tail -f log\n"********************' EXIT
-
-cd $(dirname $0)
-
-nohup ./run_script.sh &> log &
-
-tail -f log
