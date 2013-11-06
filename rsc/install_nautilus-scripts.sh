b598f3eab52b18c1acda842325b97286e7781f6e
ahheckel <heckelandreas@googlemail.com>
2013-11-06 15:54:17 +0100

    modified:   rsc/install_nautilus-scripts.sh  	modified:   rsc/scripts/nautilus-scripts/FSL_Feat

--- rsc/install_nautilus-scripts.sh
+++ rsc/install_nautilus-scripts.sh
@@ -2,8 +2,10 @@
 subdir="$1"
 cd $(dirname $0)
 installdir=~/.gnome2/nautilus-scripts/$subdir
+
 mkdir -p $installdir
-cp -sf `pwd`/scripts/nautilus-scripts/*  $installdir
-rm $installdir/env_vars
+cp -sf `pwd`/scripts/nautilus-scripts/* $installdir ; rm $installdir/env_vars
 cp `pwd`/scripts/nautilus-scripts/env_vars $installdir
+sed -i "s|FSL_DIR=.*|FSL_DIR=${FSLDIR}|g" $installdir/env_vars
+sed -i "s|FREESURFER_HOME=.*|FREESURFER_HOME=${FREESURFER_HOME}|g" $installdir/env_vars
 
