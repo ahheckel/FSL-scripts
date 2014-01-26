0430e022249d3d4d4c7ae79fe9e4883b4a43d5fd
ahheckel <heckelandreas@googlemail.com>
2014-01-26 20:05:35 +0100

    modified:   rsc/install_nautilus-scripts.sh  	modified:   rsc/scripts/main.sh  	modified:   rsc/scripts/nautilus-scripts/FREESURFER_tksurfer_fsaverage  	modified:   rsc/scripts/nautilus-scripts/FSL_Glm  	modified:   rsc/scripts/nautilus-scripts/FSL_fslview_checkFeatdir  	modified:   rsc/scripts/nautilus-scripts/env_vars  	modified:   rsc/scripts/nautilus-scripts/greperror_log

--- rsc/install_nautilus-scripts.sh
+++ rsc/install_nautilus-scripts.sh
@@ -9,4 +9,5 @@ cp -sf `pwd`/scripts/nautilus-scripts/* $installdir ; rm $installdir/env_vars
 cp `pwd`/scripts/nautilus-scripts/env_vars $installdir
 sed -i "s|FSL_DIR=.*|FSL_DIR=${FSLDIR}|g" $installdir/env_vars
 sed -i "s|FREESURFER_HOME=.*|FREESURFER_HOME=${FREESURFER_HOME}|g" $installdir/env_vars
+sed -i "s|scriptdir=.*|scriptdir=`pwd`/scripts|g" $installdir/env_vars
 echo "$(basename $0): installed scripts in '$installdir' - done."
--- rsc/scripts/main.sh
+++ rsc/scripts/main.sh
@@ -1743,7 +1743,7 @@ if [ $RECON_STG2 -eq 1 ] ; then
       
       echo '#!/bin/bash' > $fldr/recon-all_cuda.sh
       echo 'cudadetect &>/dev/null' >>  $fldr/recon-all_cuda.sh
-      echo "if [ \$? = $exitflag ] ; then recon-all -all -use-gpu -no-isrunning -noappend -clean-tal -tal-check $opts -subjid $(subjsess)" >> $fldr/recon-all_cuda.sh # you may want to remove clean-tal flag (!)
+      echo "if [ \$? = $exitflag ] ; then recon-all -all -subjid $(subjsess) -use-gpu -no-isrunning -noappend -clean-tal -tal-check $opts" >> $fldr/recon-all_cuda.sh # you may want to remove clean-tal flag (!)
       echo "else  recon-all -all -subjid $(subjsess) -no-isrunning -noappend -clean-tal -tal-check $opts ; fi" >> $fldr/recon-all_cuda.sh
       chmod +x $fldr/recon-all_cuda.sh
       
