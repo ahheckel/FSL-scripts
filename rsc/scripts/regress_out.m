106acc1ed3a6050b234d24f1b708c517ef9d6bb8
ahheckel <heckelandreas@googlemail.com>
2015-01-25 00:28:28 +0100

    modified:   rsc/scripts/regress_out.m  	modified:   rsc/update.sh

--- rsc/scripts/regress_out.m
+++ rsc/scripts/regress_out.m
@@ -1,8 +1,17 @@
-function r=regress_out(indata, confounds)
+% Written by Andreas Heckel
+% University of Heidelberg
+% heckelandreas@googlemail.com
+% https://github.com/ahheckel
+%
+% r=regress_out(indata, confounds)
+% columns=variables
+% rows=observations
+% Example: 
+%  FA=[0.5, 0.6, 0.8, ... ]' 
+%  age=[54, 20, 44, ...]'
+%  FA_corr=regress_out(FA,age)
 
-%r=regress_out(indata, confounds)
-%columns=variables
-%rows=observations
+function r=regress_out(indata, confounds)
 
 data=indata - repmat(mean(indata,1), [size(indata,1),1]);
 conf=confounds - repmat(mean(confounds,1), [size(confounds,1),1]);
--- rsc/update.sh
+++ rsc/update.sh
@@ -2,13 +2,13 @@
 # Installs required files.
 # CAVE: for AFNI tools: may need to install or link /usr/lib[64]/libXp.so.6
 
-#set -e
 cd $(dirname $0)
 if [ $# -lt 1 ] ; then echo "Usage: update [32|64]" ; exit 1 ; fi
 if [ x$FSLDIR = "x" ] ; then echo "FSLDIR variable is not defined ! Exiting." ; exit 1 ; fi
 if [ x$FREESURFER_HOME = "x" ] ; then echo "FREESURFER_HOME variable is not defined ! Exiting." ; exit 1 ; fi
 
 clear
+
 # display dir. variables
 echo ""
 echo "FSLDIR:                   '$FSLDIR'"
@@ -26,6 +26,7 @@ echo ""
 
 #cp -iv fs/trac-all $FREESURFER_HOME/bin/trac-all
 cp -iv fsl/fsl5/fsl_sub_v5_patched $FSLDIR/bin/fsl_sub # contains a RAM limit and JOB-ID redirection, should also work for FSL < v.5
+chmod +x $FSLDIR/bin/fsl_sub
 cp -iv fsl/templates/MNI152*.nii.gz $FSLDIR/data/standard/
 cp -iv fsl/templates/avg152T1*.nii.gz $FSLDIR/data/standard/
 cp -iv fsl/templates/rsn*.nii.gz $FSLDIR/data/standard/
@@ -34,26 +35,39 @@ cp -iv fsl/templates/rsn_labels* $FSLDIR/data/standard/
 
 if [ $v5 -eq 0 ] ; then # dont overwrite for fsl ver. 5
   cp -iv fsl/fsl4/tbss_x/tbss_x $FSLDIR/bin/tbss_x
+  chmod +x $FSLDIR/bin/tbss_x   
   cp -iv fsl/fsl4/topup/b02b0.cnf $FSLDIR/etc/flirtsch/b02b0.cnf
   cp -iv fsl/fsl4/featlib_v4.tcl $FSLDIR/tcl/featlib.tcl
   cp -iv fsl/fsl4/slices_summary $FSLDIR/bin/ # this one is needed for FSLNets
+  chmod +x $FSLDIR/bin/slices_summary
 fi
 
 for bit in 32 64 ; do
   if [ $1 -eq ${bit} ] ; then
     if [ $v5 -eq 0 ] ; then # dont overwrite for fsl ver. 5
       cp -iv fsl/fsl4/topup/topup_${bit} $FSLDIR/bin/topup
-      cp -iv fsl/fsl4/topup/applytopup_${bit} $FSLDIR/bin/applytopup 
+      chmod +x $FSLDIR/bin/topup
+      cp -iv fsl/fsl4/topup/applytopup_${bit} $FSLDIR/bin/applytopup
+      chmod +x $FSLDIR/bin/applytopup
       cp -iv fsl/fsl4/tbss_x/swap_voxelwise_${bit} $FSLDIR/bin/swap_voxelwise
-      cp -iv fsl/fsl4/tbss_x/swap_subjectwise_${bit} $FSLDIR/bin/swap_subjectwise
+      chmod +x $FSLDIR/bin/swap_voxelwise
+      cp -iv fsl/fsl4/tbss_x/swap_subjectwise_${bit} $FSLDIR/bin/swap_subjectwise      
+      chmod +x $FSLDIR/bin/swap_subjectwise
     fi  
     cp -iv afni/3dDespike_${bit} $FSLDIR/bin/3dDespike
+    chmod +x $FSLDIR/bin/3dDespike
     cp -iv afni/3dTcat_${bit} $FSLDIR/bin/3dTcat
+    chmod +x $FSLDIR/bin/3dTcat
     cp -iv afni/3dTstat_${bit} $FSLDIR/bin/3dTstat
+    chmod +x $FSLDIR/bin/3dTstat
     cp -iv afni/3dcalc_${bit} $FSLDIR/bin/3dcalc
+    chmod +x $FSLDIR/bin/3dcalc
     cp -iv afni/3dDetrend_${bit} $FSLDIR/bin/3dDetrend
+    chmod +x $FSLDIR/bin/3dDetrend
     cp -iv scripts/bin/sort8_${bit} scripts/bin/sort8
+    chmod +x scripts/bin/sort8
     cp -iv scripts/bin/fslmaths5_${bit} scripts/bin/fslmaths5
+    chmod +x scripts/bin/fslmaths5
   fi
 done
 
@@ -72,22 +86,7 @@ done
   #cp -iv scripts/bin/sort8_32 scripts/bin/sort8
   #cp -iv scripts/bin/fslmaths_32 scripts/bin/fslmaths5
 #fi
-
-chmod +x $FSLDIR/bin/topup
-chmod +x $FSLDIR/bin/applytopup
-chmod +x $FSLDIR/bin/swap_voxelwise
-chmod +x $FSLDIR/bin/swap_subjectwise
-chmod +x $FSLDIR/bin/tbss_x
 #chmod +x $FREESURFER_HOME/bin/trac-all
-chmod +x $FSLDIR/bin/3dTcat
-chmod +x $FSLDIR/bin/3dDespike
-chmod +x $FSLDIR/bin/3dTstat
-chmod +x $FSLDIR/bin/3dcalc
-chmod +x $FSLDIR/bin/3dDetrend
-chmod +x $FSLDIR/bin/fsl_sub
-chmod +x $FSLDIR/bin/slices_summary
-chmod +x scripts/bin/fslmaths5
-chmod +x scripts/bin/sort8
 
 if [ ! -d $FREESURFER_HOME/subjects/fsaverage/tmp ] ; then
   mkdir $FREESURFER_HOME/subjects/fsaverage/tmp
