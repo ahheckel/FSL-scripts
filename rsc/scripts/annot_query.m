1739ac7dbedbb5c0deed9e0e37030bafa9769935
ahheckel <heckelandreas@googlemail.com>
2015-05-19 19:25:34 +0200

    modified:   rsc/scripts/annot_query.m

--- rsc/scripts/annot_query.m
+++ rsc/scripts/annot_query.m
@@ -6,14 +6,15 @@ function f = annot_query(labelfile, annotfile, out)
 % https://github.com/ahheckel
 % 05/18/2015
 
-%labelfile='/home/andi/projects/a.label'
-%annotfile='/usr/local/freesurfer/subjects/fsaverage/label/lh.aparc.a2009s.annot'
+%labelfile='/home/andi/projects/biller/cr.label';
+%annotfile='/usr/local/freesurfer/subjects/fsaverage/label/lh.aparc.a2009s.annot';
 %out='a.txt';
 
-if(nargin ~= 3)
-  fprintf('l = annot_query(labelfile, annotfile, out)\n');
-  return;
-end
+% 
+% if(nargin ~= 3)
+%   fprintf('l = annot_query(labelfile, annotfile, out)\n');
+%   return;
+% end
 
 % delete existing output file
 system(sprintf('rm -f %s', out));
@@ -21,7 +22,10 @@ system(sprintf('rm -f %s', out));
 % open output file
 fid=fopen(out, 'a');
 
-v_l=read_label_new(labelfile); % read_label_new was adapted to accept the file path as argument
+% read label
+v_l=read_label_new(labelfile); % Freesurfer's read_label.m was adapted to accept the filepath as argument
+
+% read annotation
 [v,l,c]=read_annotation(annotfile,0);
 
 % 0-based -> 1-based indices
@@ -30,18 +34,33 @@ idx_l=zeros(length(v),1);
 idx_l(v_l)=1;
 
 % cycle through anatomical labels
+j=0;
 for i=1:c.numEntries
   struct_id=c.table(i,5);
   idx_s=l==struct_id;
   idx=idx_l & idx_s;
   prop(i)=sum(idx)/sum(idx_l);
   if prop(i) > 0
-    disp(sprintf('%s : %s : %.1f\n', labelfile, c.struct_names{i}, prop(i)))
-    fprintf(fid,'%s : %s : %.1f\n', labelfile, c.struct_names{i}, prop(i));
+    j=j+1;     
+    lfiles{j}=labelfile;
+    props(j)=prop(i);
+    annots{j}=c.struct_names{i};
+  
   end
 end
-disp(sprintf('%s : undefined : %.1f\n', labelfile, 1-length(v_l)/sum(idx_l)))
-fprintf(fid,'%s : undefined : %.1f\n', labelfile, 1-length(v_l)/sum(idx_l));
+
+% sort (highest percentage first)
+[props, idx]=sort(props, 'descend');
+lfiles=lfiles(idx);
+annots=annots(idx);
+
+% output
+for i=1:j
+    fprintf(fid,'%s : %s : %5.1f %%\n', lfiles{i}, annots{i}, props(i)*100);
+    disp(sprintf('%s : %s : %5.1f %%\n', lfiles{i}, annots{i}, props(i)*100));
+end
+fprintf(fid,'%s : undefined : %5.1f %%\n', labelfile, 100-length(v_l)/sum(idx_l)*100);
+disp(sprintf('%s : undefined : %5.1f %%\n', labelfile, 100-length(v_l)/sum(idx_l)*100));
 
 % close output file
 fclose(fid);
