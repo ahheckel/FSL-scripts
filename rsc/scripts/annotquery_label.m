c55a2f3e0da730b1204187856a25669ede47d048
ahheckel <heckelandreas@googlemail.com>
2015-05-20 09:20:58 +0200

    renamed:    rsc/scripts/annot_query.m -> rsc/scripts/annotquery_label.m  	new file:   rsc/scripts/annotquery_sig.m  	modified:   rsc/scripts/nautilus-scripts/FREESURFER_tksurfer

--- rsc/scripts/annotquery_label.m
+++ rsc/scripts/annotquery_label.m
@@ -1,4 +1,4 @@
-function f = annot_query(labelfile, annotfile, out)
+function f = annotquery_label(labelfile, annotfile, out)
 
 % Written by Andreas Heckel
 % University of Freiburg
@@ -6,13 +6,13 @@ function f = annot_query(labelfile, annotfile, out)
 % https://github.com/ahheckel
 % 05/18/2015
 
-%labelfile='/home/andi/projects/biller/cr.label';
-%annotfile='/usr/local/freesurfer/subjects/fsaverage/label/lh.aparc.a2009s.annot';
-%out='a.txt';
+%  labelfile='/home/andi/projects/biller/cr.label';
+%  annotfile='/usr/local/freesurfer/subjects/fsaverage/label/lh.aparc.a2009s.annot';
+%  out='a.txt';
 
 % 
 % if(nargin ~= 3)
-%   fprintf('l = annot_query(labelfile, annotfile, out)\n');
+%   fprintf('l = annotquery_label(labelfile, annotfile, out)\n');
 %   return;
 % end
 
@@ -40,27 +40,31 @@ for i=1:c.numEntries
   idx_s=l==struct_id;
   idx=idx_l & idx_s;
   prop(i)=sum(idx)/sum(idx_l);
+  propinv(i)=sum(idx)/sum(idx_s);
   if prop(i) > 0
     j=j+1;     
     lfiles{j}=labelfile;
     props(j)=prop(i);
-    annots{j}=c.struct_names{i};
-  
+    propinvs(j)=propinv(i);
+    annots{j}=c.struct_names{i};  
   end
 end
 
 % sort (highest percentage first)
 [props, idx]=sort(props, 'descend');
+propinvs=propinvs(idx);
 lfiles=lfiles(idx);
 annots=annots(idx);
 
 % output
+fprintf(fid,'input\t anat\t n_input/n_anat*100\t n_anat/n_input*100\n');
+disp(sprintf('input\t anat\t n_input/n_anat*100\t n_anat/n_input*100'));
 for i=1:j
-    fprintf(fid,'%s : %s : %5.1f %%\n', lfiles{i}, annots{i}, props(i)*100);
-    disp(sprintf('%s : %s : %5.1f %%\n', lfiles{i}, annots{i}, props(i)*100));
+    fprintf(fid,'%s: %s: %5.1f %5.1f\n', lfiles{i}, annots{i}, props(i)*100, propinvs(i)*100);
+    disp(sprintf('%s: %s: %5.1f %5.1f', lfiles{i}, annots{i}, props(i)*100, propinvs(i)*100));
 end
-fprintf(fid,'%s : undefined : %5.1f %%\n', labelfile, 100-length(v_l)/sum(idx_l)*100);
-disp(sprintf('%s : undefined : %5.1f %%\n', labelfile, 100-length(v_l)/sum(idx_l)*100));
+fprintf(fid,'%s: undefined: %5.1f\n', lfiles{i}, 100-sum(props)*100);
+disp(sprintf('%s: undefined: %5.1f', lfiles{i}, 100-sum(props)*100));
 
 % close output file
 fclose(fid);
---
+++ rsc/scripts/annotquery_sig.m
@@ -0,0 +1,73 @@
+function f = annotquery_sig(sigfile, annotfile, tval, out)
+
+% Written by Andreas Heckel
+% University of Freiburg
+% heckelandreas@googlemail.com
+% https://github.com/ahheckel
+% 05/20/2015
+
+%  sigfile='/home/andi/projects/biller/sig.mgh';
+%  annotfile='/usr/local/freesurfer/subjects/fsaverage/label/lh.aparc.a2009s.annot';
+%  out='a.txt';
+%  tval=-1.5 ;
+
+% 
+% if(nargin ~= 4)
+%   fprintf('l = annotquery_sig(sigfile, annotfile, val, out)\n');
+%   return;
+% end
+
+% delete existing output file
+system(sprintf('rm -f %s', out));
+
+% open output file
+fid=fopen(out, 'a');
+
+% read label
+v_l=load_mgh(sigfile);
+
+% read annotation
+[v,l,c]=read_annotation(annotfile,0);
+
+% create index vector
+if sign(tval)>0
+    idx_l=v_l>=tval;
+elseif sign(tval)<0
+    idx_l=v_l<=tval;
+end    
+    
+% cycle through anatomical labels
+j=0;
+for i=1:c.numEntries
+  struct_id=c.table(i,5);
+  idx_s=l==struct_id;
+  idx=idx_l & idx_s;
+  prop(i)=sum(idx)/sum(idx_l);
+  propinv(i)=sum(idx)/sum(idx_s);
+  if prop(i) > 0
+    j=j+1;     
+    lfiles{j}=sigfile;
+    props(j)=prop(i);
+    propinvs(j)=propinv(i);
+    annots{j}=c.struct_names{i};  
+  end
+end
+
+% sort (highest percentage first)
+[props, idx]=sort(props, 'descend');
+propinvs=propinvs(idx);
+lfiles=lfiles(idx);
+annots=annots(idx);
+
+% output
+fprintf(fid,'input\t anat\t n_input/n_anat*100\t n_anat/n_input*100\t t-value\n');
+disp(sprintf('input\t anat\t n_input/n_anat*100\t n_anat/n_input*100\t t-value'));
+for i=1:j
+    fprintf(fid,'%s: %s: %5.1f %5.1f %6.2f\n', lfiles{i}, annots{i}, props(i)*100, propinvs(i)*100, tval);
+    disp(sprintf('%s: %s: %5.1f %5.1f %6.2f', lfiles{i}, annots{i}, props(i)*100, propinvs(i)*100, tval));
+end
+fprintf(fid,'%s: undefined: %5.1f\n', sigfile, 100-sum(props)*100);
+disp(sprintf('%s: undefined: %5.1f', sigfile, 100-sum(props)*100));
+
+% close output file
+fclose(fid);
